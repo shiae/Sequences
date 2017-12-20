6fc0e5382e54f9263e7987d2f506a05800ac5741
Allison Shi <shiae@rose-hulman.edu>
2017-12-20 16:33:10 -0500

    m3 DONE: 4 & 5

--- src/m3_iterating.py
+++ src/m3_iterating.py
@@ -12,8 +12,8 @@ import rosegraphics as rg
 
 def main():
     """ Calls the   TEST   functions in this module. """
-    run_test_count_negatives()
-    # run_test_count_short_ones()
+    # run_test_count_negatives()
+    run_test_count_short_ones()
     # run_test_draw_circles()
 
 
@@ -129,7 +129,7 @@ def count_negatives(seq):
 def run_test_count_short_ones():
     """ Tests the   count_short_ones   function. """
     # ------------------------------------------------------------------
-    # TODO: 4. Implement this TEST function.
+    # DONE: 4. Implement this TEST function.
     #   It TESTS the  count_short_ones  function defined below.
     #   Include at least ** 2 ** ADDITIONAL tests beyond those we wrote.
     #
@@ -204,6 +204,22 @@ def run_test_count_short_ones():
 
     # TO DO 4 (continued):  Add your 2 ADDITIONAL test(s) here:
 
+    # Test 8:
+    expected = 1
+    seq = ['this', 'is', 'test', 'number', 'eight']
+    actual = count_short_ones(seq)
+    print()
+    print('Test 8 expected:', expected)
+    print('       actual:  ', actual)
+
+    # Test 9:
+    expected = 2
+    seq = [[0, 1], [20, 23, 24, 19], [900, 50, 30, 33, 35, 22], [3]]
+    actual = count_short_ones(seq)
+    print()
+    print('Test 9 expected:', expected)
+    print('       actual:  ', actual)
+
 
 def count_short_ones(seq_of_lists):
     """
@@ -226,9 +242,17 @@ def count_short_ones(seq_of_lists):
       :type seq_of_lists: (list | tuple) of (list | tuple | str)
     """
     # ------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # DONE: 5. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     # ------------------------------------------------------------------
+
+    count = 0
+
+    for k in range(len(seq_of_lists)):
+        if len(seq_of_lists[k]) < 3:
+            count += 1
+
+    return count
 
 
 def run_test_draw_circles():
