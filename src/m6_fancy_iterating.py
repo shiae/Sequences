962268738364f3ac84de8f276939a955e4b2f71f
Allison Shi <shiae@rose-hulman.edu>
2018-01-03 14:59:10 -0500

    m6 DONE: 6

--- src/m6_fancy_iterating.py
+++ src/m6_fancy_iterating.py
@@ -49,8 +49,8 @@ def main():
 
     # run_test_print_all_items_forwards(sequence1, sequence2, sequence3, sequence4)
     # run_test_print_all_items_backwards(sequence1, sequence2, sequence3, sequence4)
-    run_test_print_items_at_odd_indices(sequence1, sequence2, sequence3, sequence4)
-#     run_test_print_items_in_second_half(sequence1, sequence2, sequence3, sequence4)
+    # run_test_print_items_at_odd_indices(sequence1, sequence2, sequence3, sequence4)
+    run_test_print_items_in_second_half(sequence1, sequence2, sequence3, sequence4)
 #     run_test_print_items_that_are_bigger_than_5()  # Uses different sequences
 #     run_test_print_items_that_are_strings(sequence1, sequence2, sequence3, sequence4)
 #     run_test_print_items_that_are_odd_integers(sequence1, sequence2, sequence3, sequence4)
@@ -426,7 +426,7 @@ def print_items_in_second_half(sequence):
       100
     """
     # ------------------------------------------------------------------
-    # TODO: 6. Implement and test this function.
+    # DONE: 6. Implement and test this function.
     #          Tests have been written for you (above).
     #
     # IMPORTANT: Don't get hung up on dealing with the middle item
@@ -437,6 +437,8 @@ def print_items_in_second_half(sequence):
     #   Use   //   for integer division.
     # ------------------------------------------------------------------
 
+    for k in range(len(sequence)//2, len(sequence)):
+        print(sequence[k])
 
 # ----------------------------------------------------------------------
 # Iterating through a sequence, selecting items:
